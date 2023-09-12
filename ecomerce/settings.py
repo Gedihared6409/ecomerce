417f7c046b408c1f2ea6401be7910b46a83974e0
Gedihared6409 <59908319+Gedihared6409@users.noreply.github.com>
2023-09-12 20:23:11 +0900

    Update settings.py

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 import dj_database_url
 from decouple import config,Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -104,7 +104,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['ecomerceweb.herokuapp.com','127.0.0.1','ecomerce.up.railway.app']
+ALLOWED_HOSTS = ['*']
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
