234ba9d27a47d18a674ee17d11f1d057166c8bd7
Gedihared6409 <59908319+Gedihared6409@users.noreply.github.com>
2023-09-13 15:54:42 +0900

    Update settings.py

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -31,7 +31,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -104,7 +104,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['*']
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
